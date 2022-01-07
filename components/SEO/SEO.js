c2286a6325758b827489b31f35cbcff2710743bf
Facundo Lavagnino <flavagnino8@gmail.com>
2022-01-07 15:31:52 -0300

    fix deploy error

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -11,13 +11,13 @@ const SEO = ({ title }) => {
 
       {/* Fonts */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet"
       />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700&display=swap"
         rel="stylesheet"
