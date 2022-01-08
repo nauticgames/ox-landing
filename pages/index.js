b215313a5542374d55fbf9e408c308fb7d08311f
Facundo Lavagnino <flavagnino8@gmail.com>
2022-01-07 22:45:39 -0300

    about game done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import About from "../components/About/About";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
 import SEO from "../components/SEO/SEO";
+import Stadiums from "../components/Stadiums/Stadiums";
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <SEO />
       <Header />
       <Hero />
+      <About />
+      <Stadiums />
     </>
   );
 }
