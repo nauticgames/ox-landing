d4e52a61d57a9d14b28cfc4cff7c0f034f0c82c0
Facundo Lavagnino <flavagnino8@gmail.com>
2022-04-01 00:33:26 -0300

    section title changed

--- components/Roadmap/Roadmap.js
+++ components/Roadmap/Roadmap.js
@@ -8,67 +8,59 @@ const Roadmap = () => {
       <div className="wrapped">
         <SectionTitle title="Roadmap 2022" />
         <div className="roadmap__cards">
-          <WithTopFade>
-            <div className="card">
-              <div className="title">Q1</div>
-              <div className="card__bg">
-                <span className="blue"></span>
-                <span className="red"></span>
-              </div>
-              <p>
-                Stadiums public sale <span>&#10003;</span>
-              </p>
-              <p>
-                Marketplace <span>&#10003;</span>
-              </p>
-              <p>
-                Whitepaper <span>&#10003;</span>
-              </p>
-              <p>Marketing campaigns</p>
+          <div className="card">
+            <div className="title">Q1</div>
+            <div className="card__bg">
+              <span className="blue"></span>
+              <span className="red"></span>
             </div>
-          </WithTopFade>
-          <WithTopFade>
-            <div className="card">
-              <div className="title">Q2</div>
-              <p>Game demo</p>
-              <p>Team recruitment</p>
-              <p>OXS public sale</p>
-              <p>First Mystery Chests for sale</p>
-              <p>Launch Closed beta PC version with Normal Games</p>
-              <div className="card__bg">
-                <span className="blue"></span>
-                <span className="red"></span>
-              </div>
+            <p>
+              Stadiums public sale <span>&#10003;</span>
+            </p>
+            <p>
+              Marketplace <span>&#10003;</span>
+            </p>
+            <p>
+              Whitepaper <span>&#10003;</span>
+            </p>
+            <p>Marketing campaigns</p>
+          </div>
+          <div className="card">
+            <div className="title">Q2</div>
+            <p>Game demo</p>
+            <p>Team recruitment</p>
+            <p>OXS public sale</p>
+            <p>First Mystery Chests for sale</p>
+            <p>Launch Closed beta PC version with Normal Games</p>
+            <div className="card__bg">
+              <span className="blue"></span>
+              <span className="red"></span>
             </div>
-          </WithTopFade>
-          <WithTopFade>
-            <div className="card">
-              <div className="title">Q3</div>
-              <p>Open PC version with competitive</p>
-              <p>Launch Lunary in mainnet</p>
-              <p>Claim rewards</p>
-              <p>Pass Sales 5v5 Solo/Duo, 1v1 Silver</p>
-              <p>More chests for sale</p>
-              <div className="card__bg">
-                <span className="blue"></span>
-                <span className="red"></span>
-              </div>
+          </div>
+          <div className="card">
+            <div className="title">Q3</div>
+            <p>Open PC version with competitive</p>
+            <p>Launch Lunary in mainnet</p>
+            <p>Claim rewards</p>
+            <p>Pass Sales 5v5 Solo/Duo, 1v1 Silver</p>
+            <p>More chests for sale</p>
+            <div className="card__bg">
+              <span className="blue"></span>
+              <span className="red"></span>
             </div>
-          </WithTopFade>
-          <WithTopFade>
-            <div className="card">
-              <div className="title">Q4</div>
-              <p>Teams mode</p>
-              <p>5v5 Teams pass</p>
-              <p>Community Tournaments</p>
-              <p>Android & iOS release</p>
-              <p>First official competitive leagues</p>
-              <div className="card__bg">
-                <span className="blue"></span>
-                <span className="red"></span>
-              </div>
+          </div>
+          <div className="card">
+            <div className="title">Q4</div>
+            <p>Teams mode</p>
+            <p>5v5 Teams pass</p>
+            <p>Community Tournaments</p>
+            <p>Android & iOS release</p>
+            <p>First official competitive leagues</p>
+            <div className="card__bg">
+              <span className="blue"></span>
+              <span className="red"></span>
             </div>
-          </WithTopFade>
+          </div>
         </div>
       </div>
     </section>
--- components/Tokenomics/Tokenomics.js
+++ components/Tokenomics/Tokenomics.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 import { WithLeftFade, WithRightFade } from "../../hoc/WithFade";
 import OXSDistribution from "../Charts/OXSDistribution";
 import SectionTitle from "../UI/SectionTitle/SectionTitle";
@@ -9,50 +8,59 @@ const Tokenomics = () => {
     <section className="tokenomics__section" id="tokenomics">
       <div className="wrapped">
         <SectionTitle title="Token economics" />
-
-        <WithLeftFade>
-          <div className="token__card">
-            <div className="token__img">
-              <img src="/assets/lunary.png" alt="lunary icon" loading="lazy" />
-            </div>
-            <div className="token__info">
-              <h2>LUNARY ($LUNY)</h2>
-              <p>
-                Lunary is the utility token of the OX ecosystem, it is an
-                inflationary token, infinite, with burning methods, it would be
-                used to enter the competitive mode of the game, for each game
-                won, stadium rewards, to open chests with skins, to advertise
-                in-game and to enter community tournaments. We will give it more
-                uses in the future.
-              </p>
+        <div className="token__card">
+          <div className="token__img">
+            <Image
+              src="/assets/lunary.png"
+              alt="lunary token icon"
+              priority
+              objectFit="contain"
+              quality={100}
+              layout="fill"
+            />
+          </div>
+          <div className="token__info">
+            <h2>LUNARY ($LUNY)</h2>
+            <p>
+              Lunary is the utility token of the OX ecosystem, it is an
+              inflationary token, infinite, with burning methods, it would be
+              used to enter the competitive mode of the game, for each game won,
+              stadium rewards, to open chests with skins, to advertise in-game
+              and to enter community tournaments. We will give it more uses in
+              the future.
+            </p>
+          </div>
+        </div>
+        <div className="token__card">
+          <div className="token__img">
+            <Image
+              src="/assets/oxs.png"
+              alt="ox soccer token icon"
+              priority
+              objectFit="contain"
+              quality={100}
+              layout="fill"
+            />
+          </div>
+          <div className="token__info">
+            <h2>OX SOCCER ($OXS)</h2>
+            <p>
+              OXS is the main token of the OX project and ecosystem, a token
+              used especially for future governance. With this token we plan to
+              decentralize decision making about the game in the future. The
+              people who own this token will have the possibility to vote
+              changes in the project, the rewards in the official tournaments
+              will be with this token, the best players of each season will also
+              be rewarded with OXS, and the brands will be able to publish
+              in-game with this token.
+            </p>
+            <div className="oxs__distribution">
+              <h2>OX Soccer token distribution</h2>
+              <h3>300M total supply</h3>
+              <OXSDistribution />
             </div>
           </div>
-        </WithLeftFade>
-        <WithRightFade>
-          <div className="token__card">
-            <div className="token__img">
-              <img src="/assets/oxs.png" alt="oxs icon" loading="lazy" />
-            </div>
-            <div className="token__info">
-              <h2>OX SOCCER ($OXS)</h2>
-              <p>
-                OXS is the main token of the OX project and ecosystem, a token
-                used especially for future governance. With this token we plan
-                to decentralize decision making about the game in the future.
-                The people who own this token will have the possibility to vote
-                changes in the project, the rewards in the official tournaments
-                will be with this token, the best players of each season will
-                also be rewarded with OXS, and the brands will be able to
-                publish in-game with this token.
-              </p>
-              <div className="oxs__distribution">
-                <h2>OX Soccer token distribution</h2>
-                <h3>300M total supply</h3>
-                <OXSDistribution />
-              </div>
-            </div>
-          </div>
-        </WithRightFade>
+        </div>
       </div>
     </section>
   );
